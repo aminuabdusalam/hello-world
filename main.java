a5d8b6325e12dbb32f30cdbee5b318a4fca3f801
aminuabdusalam <60335691+aminuabdusalam@users.noreply.github.com>
2020-07-13 08:45:54 -0500

    Short description to the world

---
+++ main.java
@@ -0,0 +1,2 @@
+System.out.println("Hello world! Aminu here. Follow me in journey into becoming a successful individual at what I do. 
+Give me a chance and I would show you why I will be an invaluable addition to your firm.")
